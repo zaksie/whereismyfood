dbfe6c89db04209609f29dbed578a7a012ad5b51
Zak Goichman <zak.goichman@similarweb.com>
2017-01-17 19:20:33 +0200

    more stuff...

--- src/main/scala/info/whereismyfood/routes/api/v1/http/UserRoutes.scala
+++ src/main/scala/info/whereismyfood/routes/api/v1/http/UserRoutes.scala
@@ -1,10 +1,13 @@
 package info.whereismyfood.routes.api.v1.http
 
+import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.server.Directives._
 import info.whereismyfood.aux.ActorSystemContainer.Implicits._
 import info.whereismyfood.modules.device.DeviceInfoJson
 import info.whereismyfood.modules.user.{Creds, GenericUser, UserRouter}
 import org.slf4j.LoggerFactory
+
+import scala.util.Random
 /**
   * Created by zakgoichman on 10/21/16.
   */
@@ -22,10 +25,17 @@ object UserRoutes {
         }
       }
     } ~
-        path("me") {
-          GenericUser.getById(creds.phone) match {
-            case Some(user) => complete(user.getInfoJson)
-            case _ => complete(503)
+        pathPrefix("me") {
+          pathEndOrSingleSlash {
+            GenericUser.getById(creds.phone) match {
+              case Some(user) => complete(user.getInfoJson)
+              case _ => complete(503)
+            }
+          } ~
+          path("finances"){
+            //TODO: Stub
+            val r = Random.nextInt(100000)/100f
+            complete(s"""{"runningTotal": $r}""")
           }
         } ~
     pathPrefix("device"){
